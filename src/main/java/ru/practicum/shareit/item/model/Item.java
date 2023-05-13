e7bdab6a3488e63c187ddbd811cd5c9be7a5422a
Terekhoff Sasha <terekhoff82@mail.ru>
2023-05-14 03:32:21 +0400

    feat: добавление маппера для сущностей Item и User.

--- src/main/java/ru/practicum/shareit/item/model/Item.java
+++ src/main/java/ru/practicum/shareit/item/model/Item.java
@@ -1,18 +1,20 @@
 package ru.practicum.shareit.item.model;
 
-import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 import ru.practicum.shareit.request.ItemRequest;
 import ru.practicum.shareit.user.User;
 
 /**
  * TODO Sprint add-controllers.
  */
-@Data
+@Getter
+@Setter
 public class Item {
     private Integer id;
     private String name;
     private String description;
-    private Boolean isAvailable;
+    private Boolean available;
     private User owner;
     private ItemRequest request;
 }
