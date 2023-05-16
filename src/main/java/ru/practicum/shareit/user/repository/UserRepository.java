60347e9f5f211d9eaf211f5ddffe3c2d48394891
Terekhoff Sasha <terekhoff82@mail.ru>
2023-05-17 00:30:09 +0400

    fix: code style.

--- src/main/java/ru/practicum/shareit/user/repository/UserRepository.java
+++ src/main/java/ru/practicum/shareit/user/repository/UserRepository.java
@@ -6,8 +6,12 @@ import java.util.List;
 
 public interface UserRepository {
     User create(User user);
+
     User update(User user);
+
     User findOne(Integer id);
+
     List<User> findAll();
+
     void delete(Integer id);
 }
