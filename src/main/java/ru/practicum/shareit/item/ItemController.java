d28d8ae890231364dcb6d2e55a4edc97e2f4fe7b
Terekhoff Sasha <terekhoff82@mail.ru>
2023-05-22 02:12:10 +0400

    fix: изменение валидации при обновлении вещи.

--- src/main/java/ru/practicum/shareit/item/ItemController.java
+++ src/main/java/ru/practicum/shareit/item/ItemController.java
@@ -24,7 +24,7 @@ public class ItemController {
     }
 
     @PatchMapping("/{itemId}")
-    public ItemDto update(@Valid @RequestBody ItemDto dto, @PathVariable Integer itemId, @RequestHeader(USER_ID) Integer userId) {
+    public ItemDto update(@RequestBody ItemDto dto, @PathVariable Integer itemId, @RequestHeader(USER_ID) Integer userId) {
         log.info(LogMessage.PATCH_REQUEST);
         return itemService.update(dto, itemId, userId);
     }
