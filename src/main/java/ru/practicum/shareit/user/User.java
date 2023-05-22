bd8f514296fd74e41dd7186d8671dc008fc3656d
Terekhoff Sasha <terekhoff82@mail.ru>
2023-05-23 03:19:28 +0400

    fix: убирает не используемые аннотации.

--- src/main/java/ru/practicum/shareit/user/User.java
+++ src/main/java/ru/practicum/shareit/user/User.java
@@ -3,16 +3,10 @@ package ru.practicum.shareit.user;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
-import javax.validation.constraints.Email;
-import javax.validation.constraints.NotNull;
-
 @Data
 @AllArgsConstructor
 public class User {
     private Integer id;
-    @NotNull
     private String name;
-    @Email
-    @NotNull
     private String email;
 }
